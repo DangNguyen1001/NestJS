5db7b5cf12e17ad83c04f6a464afc673e8b3a81f
Nguyễn Huỳnh Minh Quân <nhmdang1819@gmail.com>
2020-04-24 12:46:41 +0700

    Try connect to Mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './products/products.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductModule],
-  controllers: [AppController],
+  imports: [TypeOrmModule.forRoot()],
   providers: [AppService],
+  controllers: [AppController],
 })
 export class AppModule {}
